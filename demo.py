afe748c8c6c7a82c299b1db656cc1b4bae2b7c56
Ektoras Rontos <rontekto@gmail.com>
2022-06-20 17:02:52 +0300

    Init

---
+++ demo.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+
+import src
+
+data = np.load('hw3.npy', allow_pickle = True)
+
+"""
+verts3d = data[()]['verts3d']
+faces = data[()]['faces']
+vcolors = data[()]['vcolors']
+c_org = data[()]['c_org']
+c_lookat = data[()]['c_lookat']
+c_up = data[()]['c_up']
+t_1 = data[()]['t_1']
+t_2 = data[()]['t_2']
+u = data[()]['u']
+phi = data[()]['phi']
+"""
---
+++ src.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# Conf
+IMG_WIDTH = 512
+
+def ambient_light(k, I):
+    """
+        Do What
+
+        Arguments:
+            k:
+            I:
+        
+        Returns:
+            what
+    """
+    
+    pass
