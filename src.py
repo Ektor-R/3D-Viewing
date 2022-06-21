578ef8cbdc01dcf3b9964b26ac38413f7469c39b
Ektoras Rontos <rontekto@gmail.com>
2022-06-21 23:15:24 +0300

    Specular light

--- src.py
+++ src.py
@@ -67,3 +67,62 @@ def diffuse_light(
         # TODO reflection larger than 1
 
         return color + np.sum(reflections, axis=0)
+
+
+
+def specular_light(
+        point: np.ndarray,
+        N: np.ndarray, 
+        color: np.ndarray, 
+        cameraPosition: np.ndarray, 
+        k: float,
+        n: float,
+        lightPositions: np.ndarray,
+        lightIntensities: np.ndarray
+    ) -> np.ndarray:
+        """
+            Calculate specular reflection on point
+
+            Arguments:
+                point: Point to calculate reflection on.
+                N: Surface perpendicular vector on point.
+                color: Color of point.
+                cameraPosition: Camera center coordinates
+                k: Light reflection coefficient.
+                n: Phong coefficient.
+                lightPositions: List with coordinates of light sources.
+                lightIntensities: List with light intensity of each color source
+                
+            Returns:
+                color of point including light reflection 
+        """
+        
+        # If lightPositions is vector, make it array.
+        if lightPositions.ndim == 1:
+            lightPositions = lightPositions[None]
+        if lightIntensities.ndim == 1:
+            lightIntensities = lightIntensities[None]
+
+        # Calculate unit vectors of each light source with point as the start
+        lightVectors = lightPositions - point
+        # TODO lightVector = 0 (point is a source)
+        lightVectors = lightVectors / np.linalg.norm(lightVectors, axis=1)[:,None]
+
+        # Calculate unit vector of camera center with point as the start
+        cameraVector = cameraPosition - point
+        # TODO cameraVector = 0 (point is camera center)
+        cameraVector = cameraVector / np.linalg.norm(cameraVector)
+
+        lightAngles = np.einsum(
+                'j,ij->i', 
+                cameraVector, 
+                2 * N * np.einsum('j,ij->i', N, lightVectors)[:,None] - lightVectors
+            )
+
+        # Reflection created by each light source
+        reflections = lightIntensities * k * (lightAngles[:,None]**n)
+
+        # TODO reflection larger than 1
+
+        return color + np.sum(reflections, axis=0)
+
